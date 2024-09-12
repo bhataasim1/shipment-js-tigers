96bc8027f0e53f80103866fbcbf77ac0fd72305b
bhataasim1 <bhataasim700@gmail.com>
2024-09-12 22:11:37 +0530

    Add AnalyticsPage component to display the analytics section in the dashboard

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnalyticsPage = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <h1 className="text-2xl font-semibold">Analytics</h1>
+    </div>
+  );
+};
+
+export default AnalyticsPage;
