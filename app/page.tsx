6ceff36292db4b8de8225eb4c62fcbd22cb43fb9
bhataasim1 <bhataasim700@gmail.com>
2024-09-12 22:17:02 +0530

    Refactor home page layout and add button for dashboard navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Hi</h1>
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">
+        Welcome to Shipment JS-Tigers
+      </h1>
+      <p className="text-center mt-4">
+        Get started by Visiting{" "}
+        <code className="bg-gray-100 p-1 rounded">/dashboard</code>
+      </p>
+      <div className="flex justify-center mt-10">
+        <Link href="/dashboard">
+          <Button>Go to Dashboard</Button>
+        </Link>
+      </div>
+    </div>
   );
 }
