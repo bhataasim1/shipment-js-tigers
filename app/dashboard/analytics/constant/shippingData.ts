6bd9ad6cd9ac59ebe769725f52a4ec070a21a5af
bhataasim1 <bhataasim700@gmail.com>
2024-09-13 12:31:54 +0530

    Add shippingData constant for storing shipping stages and locations in the analytics dashboard

---
+++ app/dashboard/analytics/constant/shippingData.ts
@@ -0,0 +1,72 @@
+export const stages = [
+  {
+    title: "Loading",
+    locations: [
+      { name: "NHAVA SHEVA, INDIA", count: 378 },
+      { name: "HONG KONG, HONG KONG", count: 70 },
+      { name: "YANTIAN, CHINA", count: 48 },
+      { name: "DALIAN, CHINA", count: 29 },
+      { name: "SHANGHAI, 31, CHINA", count: 26 },
+      { name: "LONDON GATEWAY PORT, UK", count: 23 },
+      { name: "YANTIAN, RUSSIA", count: 48 },
+      { name: "DALIAN, RUSSIA", count: 29 },
+      { name: "SHANGHAI, 31, RUSSIA", count: 26 },
+      { name: "LONDON GATEWAY PORT, RUSSIA", count: 23 },
+    ],
+  },
+  {
+    title: "Discharge",
+    locations: [
+      { name: "NHAVA SHEVA, INDIA", count: 378 },
+      { name: "HONG KONG, HONG KONG", count: 70 },
+      { name: "YANTIAN, CHINA", count: 48 },
+      { name: "DALIAN, CHINA", count: 29 },
+      { name: "SHANGHAI, 31, CHINA", count: 26 },
+      { name: "LONDON GATEWAY PORT, UK", count: 23 },
+    ],
+  },
+  {
+    title: "Delivery",
+    locations: [
+      { name: "NHAVA SHEVA, INDIA", count: 378 },
+      { name: "HONG KONG, HONG KONG", count: 70 },
+      { name: "YANTIAN, CHINA", count: 48 },
+      { name: "DALIAN, CHINA", count: 29 },
+      { name: "SHANGHAI, 31, CHINA", count: 26 },
+      { name: "LONDON GATEWAY PORT, UK", count: 23 },
+    ],
+  },
+  {
+    title: "Shipper",
+    locations: [
+      { name: "NHAVA SHEVA, INDIA", count: 378 },
+      { name: "HONG KONG, HONG KONG", count: 70 },
+      { name: "YANTIAN, CHINA", count: 48 },
+      { name: "DALIAN, CHINA", count: 29 },
+      { name: "SHANGHAI, 31, CHINA", count: 26 },
+      { name: "LONDON GATEWAY PORT, UK", count: 23 },
+    ],
+  },
+  {
+    title: "Consignee",
+    locations: [
+      { name: "NHAVA SHEVA, INDIA", count: 378 },
+      { name: "HONG KONG, HONG KONG", count: 70 },
+      { name: "YANTIAN, CHINA", count: 48 },
+      { name: "DALIAN, CHINA", count: 29 },
+      { name: "SHANGHAI, 31, CHINA", count: 26 },
+      { name: "LONDON GATEWAY PORT, UK", count: 23 },
+    ],
+  },
+  {
+    title: "Carrier",
+    locations: [
+      { name: "NHAVA SHEVA, INDIA", count: 378 },
+      { name: "HONG KONG, HONG KONG", count: 70 },
+      { name: "YANTIAN, CHINA", count: 48 },
+      { name: "DALIAN, CHINA", count: 29 },
+      { name: "SHANGHAI, 31, CHINA", count: 26 },
+      { name: "LONDON GATEWAY PORT, UK", count: 23 },
+    ],
+  },
+];
