4fe67805b68fbcd9280007d66e9bd261469369a6
bhataasim1 <bhataasim700@gmail.com>
2024-09-13 16:14:36 +0530

    Refactor LatestDocuments component to fetch and display documents dynamically

--- app/dashboard/_components/LatestDocuments.tsx
+++ app/dashboard/_components/LatestDocuments.tsx
@@ -1,81 +1,73 @@
-import Image from "next/image";
-import React from "react";
+"use client";
 
-const documents = [
-  {
-    type: "Packing List",
-    jobNumber: "IM159075",
-    date: "Nov 29, 2023",
-    time: "10:27 AM",
-  },
-  {
-    type: "Commercial Invoice",
-    jobNumber: "IM159075",
-    date: "Nov 29, 2023",
-    time: "10:26 AM",
-  },
-  {
-    type: "Bill of Lading",
-    jobNumber: "IM159075",
-    date: "Nov 29, 2023",
-    time: "10:12 AM",
-  },
-  {
-    type: "Packing List",
-    jobNumber: "IM156930",
-    date: "Nov 29, 2023",
-    time: "10:11 AM",
-  },
-  {
-    type: "Commercial Invoice",
-    jobNumber: "IM156930",
-    date: "Nov 29, 2023",
-    time: "10:10 AM",
-  },
-  {
-    type: "Bill of Lading",
-    jobNumber: "IM156930",
-    date: "Nov 29, 2023",
-    time: "10:09 AM",
-  },
-];
+import { formatDate } from "@/lib/formatDate";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+interface IDocument {
+  id: number;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const LatestDocuments = () => {
+  const [documents, setDocuments] = useState<IDocument[]>([]);
+
+  const getAllDocuments = async () => {
+    try {
+      const response = await fetch("/api/documents");
+      const data = await response.json();
+      setDocuments(data.documents);
+      // console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllDocuments();
+  }, []);
+
   return (
     <div className="bg-white rounded-lg shadow-md p-4">
       <h2 className="text-lg font-semibold mb-4">Latest Documents</h2>
 
       <ul className="space-y-3 h-64 overflow-y-auto">
-        {documents.map((doc, index) => (
-          <li
-            key={index}
-            className="flex items-center space-x-3 pb-3 border-b border-gray-200"
-          >
-            <div className="flex-shrink-0">
-              <Image
-                src="/pdfIcon.png"
-                alt="pdf-icon"
-                width={25}
-                height={25}
-                priority
-              />
-            </div>
+        {documents.map((doc) => {
+          const { date, time } = formatDate(doc.updatedAt);
+          return (
+            <li
+              key={doc.id}
+              className="flex items-center space-x-3 pb-3 border-b border-gray-200"
+            >
+              <div className="flex-shrink-0">
+                <Image
+                  src="/pdfIcon.png"
+                  alt="pdf-icon"
+                  width={25}
+                  height={25}
+                  priority
+                />
+              </div>
 
-            <div className="flex-1 min-w-0">
-              <p className="text-sm font-medium text-gray-900 truncate">
-                {doc.type}
-              </p>
-              <p className="text-sm text-gray-500 truncate">
-                Sea job document ({doc.jobNumber})
-              </p>
-            </div>
+              <div className="flex-1 min-w-0">
+                <p className="text-sm font-medium text-gray-900 truncate">
+                  {doc.title}
+                </p>
+                <p className="text-sm text-gray-500 truncate">
+                  Sea job document ({doc.description})
+                </p>
+              </div>
 
-            <div className="flex-shrink-0 text-sm text-gray-500">
-              <p>{doc.date}</p>
-              <p>{doc.time}</p>
-            </div>
-          </li>
-        ))}
+              <div className="flex-shrink-0 text-sm text-gray-500">
+                <p>{date}</p>
+                <p>{time}</p>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
