45b418c78695baf34a6c6fc8da426feea213b2d7
bhataasim1 <bhataasim700@gmail.com>
2024-09-13 12:41:39 +0530

    Refactor Sidebar component to use client-side navigation and highlight active links

--- app/dashboard/_components/Sidebar.tsx
+++ app/dashboard/_components/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   HomeIcon,
   BarChartIcon,
@@ -8,26 +10,32 @@ import {
   HistoryIcon,
 } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const path = usePathname();
   return (
     <aside className="fixed w-64 bg-[#6D1D1D] text-white h-[70%] rounded-r-3xl">
       <div className="p-4">
         <nav>
           <ul className="space-y-2 mt-20">
             <li>
-              <Link
+            <Link
                 href="/dashboard"
-                className="flex items-center space-x-2 p-2 rounded-lg bg-[#8B2525]"
+                className={`flex items-center space-x-2 p-2 rounded-lg ${
+                  path === "/dashboard" ? "bg-[#8B2525]" : "hover:bg-[#8B2525]"
+                }`}
               >
                 <HomeIcon className="w-5 h-5" />
                 <span>Dashboard</span>
               </Link>
             </li>
             <li>
-              <Link
+            <Link
                 href="/dashboard/analytics"
-                className="flex items-center space-x-2 p-2 rounded-lg hover:bg-[#8B2525]"
+                className={`flex items-center space-x-2 p-2 rounded-lg ${
+                  path === "/dashboard/analytics" ? "bg-[#8B2525]" : "hover:bg-[#8B2525]"
+                }`}
               >
                 <BarChartIcon className="w-5 h-5" />
                 <span>Analytics</span>
