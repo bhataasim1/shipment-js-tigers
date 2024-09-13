750040a94e7f833766f9c0b410432144fae35545
bhataasim1 <bhataasim700@gmail.com>
2024-09-13 12:31:08 +0530

    Refactor layout component to add transport analytics page components

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import TransportNavComponent from "./_components/TransportNavComponent";
+import FilterComponent from "./_components/FilterComponent";
+import MilestonesCard from "./_components/MileStoneCard";
+import TimeLineCard from "./_components/TimeLineCard";
+import { stages } from "./constant/shippingData";
+import ShippingStageCard from "./_components/ShippingStageCard";
+import DataTable from "./_components/DataTable";
 
 const AnalyticsPage = () => {
   return (
-    <div className="flex justify-between items-center">
-      <h1 className="text-2xl font-semibold">Analytics</h1>
+    <div className="space-y-3">
+      <TransportNavComponent />
+      <FilterComponent />
+      <div className="grid grid-cols-2 gap-6">
+        <MilestonesCard />
+        <TimeLineCard />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {stages.map((stage, index) => (
+          <ShippingStageCard
+            key={index}
+            title={stage.title}
+            locations={stage.locations}
+          />
+        ))}
+      </div>
+      <DataTable />
     </div>
   );
 };
