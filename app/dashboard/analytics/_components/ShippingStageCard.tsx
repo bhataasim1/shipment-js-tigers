8b749597bbee2199d5b8f950df8ba9076f0a9933
bhataasim1 <bhataasim700@gmail.com>
2024-09-24 12:37:20 +0530

    Refactor ShippingStageCard component to optimize loading effect and add loading state

--- app/dashboard/analytics/_components/ShippingStageCard.tsx
+++ app/dashboard/analytics/_components/ShippingStageCard.tsx
@@ -2,32 +2,22 @@
 
 import React, { useEffect, useState } from "react";
 import { IShipment } from "./DataTable";
+import LoadingEffect from "@/components/common/LoadingEffect";
 
 interface ShippingStageCardProps {
   title: string;
   field: keyof IShipment;
+  shipments: IShipment[];
+  loading: boolean;
 }
 
 export default function ShippingStageCard({
   title,
   field,
+  shipments,
+  loading,
 }: ShippingStageCardProps) {
-  const [shipments, setShipments] = useState<IShipment[]>([]);
   const [countedData, setCountedData] = useState<{ [key: string]: number }>({});
-
-  const getAllShipments = async () => {
-    try {
-      const response = await fetch("/api/shipments");
-      const data = await response.json();
-      setShipments(data.shipment);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getAllShipments();
-  }, []);
 
   useEffect(() => {
     if (shipments.length > 0) {
@@ -42,11 +32,19 @@ export default function ShippingStageCard({
     }
   }, [shipments, field]);
 
+  console.log("Data counted", countedData);
+
+  if (loading) {
+    return (
+      <LoadingEffect totalNumber={1} loadingLines={7} />
+    )
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md p-6">
       <h2 className="text-xl font-semibold mb-4">{title}</h2>
       <div className="space-y-2 pr-2 max-h-64 overflow-y-auto">
-        {Object.entries(countedData).map(([key, count], index) => (
+        {countedData ? (Object.entries(countedData).map(([key, count], index) => (
           <div
             key={index}
             className="flex justify-between items-center py-1 border-b border-gray-200 last:border-b-0"
@@ -54,7 +52,9 @@ export default function ShippingStageCard({
             <span className="text-[14px] text-gray-600">{key}</span>
             <span className="text-[14px] font-medium">{count}</span>
           </div>
-        ))}
+        ))) : (
+          <LoadingEffect totalNumber={1} loadingLines={7} />
+        )}
       </div>
     </div>
   );
